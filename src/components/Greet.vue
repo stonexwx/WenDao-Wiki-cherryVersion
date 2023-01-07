ef686cd0dd9fb75aca434398b9040153ec2bd336
xwx <1162027477@qq.com>
2023-01-07 11:25:43 +0800

    Vue3+Element UI Plus

---
+++ src/components/Greet.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Cherry from 'cherry-markdown'
+import 'cherry-markdown/dist/cherry-markdown.min.css'
+import {onMounted, ref} from "vue";
+function init(){
+  new Cherry({
+    id: 'markdown-container',
+    value: '# welcome to cherry editor!',
+    toolbars: {
+      showToolbar: false,
+    },
+    previewer: {
+      enablePreviewerBubble: true,
+    }
+  })
+
+}
+onMounted(()=>{
+  init()
+})
+</script>
+
+<template>
+  <div id="markdown-container" style="height:auto"></div>
+</template>
