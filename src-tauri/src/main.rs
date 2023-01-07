2bd911c67885cd1a3efa05f54a03a8a01fa39633
xwx <1162027477@qq.com>
2023-01-08 00:02:49 +0800

    添加按钮，测试打开新的窗口还存在一些问题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,20 +4,43 @@
 )]
 
 
+use std::error::Error;
+use tauri::{Manager, WindowBuilder, WindowMenuEvent, Wry};
 use crate::create_toc::Poc;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-//目录结构解析
+
 mod create_toc;
+mod menu;
+mod file;
+
 
 #[tauri::command]
 fn create_toc(json:&str) -> String{
     Poc::create(json)
 }
 
+
 fn main() {
-    tauri::Builder::default()
+      tauri::Builder::default()
+        .menu(menu::get_menu())
+        .on_menu_event(|event|{
+            match event.menu_item_id() {
+                "open"=>{
+                    let handle = event.window().app_handle();
+                    std::thread::spawn(move || {
+                        let local_window = WindowBuilder::new(
+                            &handle,
+                            "exeee",
+                            tauri::WindowUrl::App("index.html".into())
+                        ).build().unwrap();
+                    });
+                }
+                _=>{}
+            };
+        })
         .invoke_handler(tauri::generate_handler![create_toc])
+        // .run(tauri::generate_context!())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
