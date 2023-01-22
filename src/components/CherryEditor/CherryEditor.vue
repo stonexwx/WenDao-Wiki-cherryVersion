3c595cb1f97885baa536abb39c82edb013b88fe9
xwx <1162027477@qq.com>
2023-01-22 23:31:22 +0800

    增加监听窗口所在屏幕dpi大小修改后高度数值按比例修改

--- src/components/CherryEditor/CherryEditor.vue
+++ src/components/CherryEditor/CherryEditor.vue
@@ -16,20 +16,31 @@ const height = ref(0)
 const size = appWindow.innerSize();
 const factor = appWindow.scaleFactor();
 const getSize=()=> {
+  //默认尺寸
   size.then(async Res => {
     height.value = Res.toLogical(await factor).height -27
   })
+  //监听窗口大小修改后高度数值按比例修改
   appWindow.onResized(() => {
     const size = appWindow.innerSize();
     size.then(async Res => {
       height.value = Res.toLogical(await factor).height - 27
     })
   });
+  //监听窗口所在屏幕dpi大小修改后高度数值按比例修改
+  appWindow.onScaleChanged(({payload})=>{
+    const size = appWindow.innerSize();
+    size.then(async Res => {
+      height.value = Res.toLogical(await payload.scaleFactor).height - 27
+    })
+  })
 }
 
 onMounted(()=>{
 
   getSize()
+
+  //打开文件后从缓存中读取前一个窗口从Rust事件获取到的文本信息，这个有改进空间就看Tauri后期有没有优化
   if(localStorage.getItem("text")!==null &&localStorage.getItem("text")!==""){
     CherryObjUtil.interface().setMarkdown(localStorage.getItem("text"))
     localStorage.removeItem("text")
